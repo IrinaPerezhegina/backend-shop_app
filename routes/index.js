7f73dc795fa983043da45e58b90c1cdedb0717e8
IrinaPerezhegina <perezeginaiv@mail.ru>
2023-01-31 17:14:25 +0500

    added router auth

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.use("/products", require("./products.routes"));
+router.use("/auth", require("./auth.routes"));
 
 module.exports = router;
