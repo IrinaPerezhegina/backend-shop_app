96292f830e4c418dec2720714469b680d5d005dc
IrinaPerezhegina <perezeginaiv@mail.ru>
2023-01-31 22:10:14 +0500

    added router comment

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router({ mergeParams: true });
 
 router.use("/products", require("./products.routes"));
 router.use("/auth", require("./auth.routes"));
+router.use("/comment", require("./comment.routes"));
 
 module.exports = router;
